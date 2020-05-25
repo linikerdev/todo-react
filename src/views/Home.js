94b22ea0e732f961f8a3135981588435a52575cd
Liniker Silva <contato@liniker.com.br>
2020-05-25 18:54:26 -0300

    layout base

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,24 +1,39 @@
-import React from 'react';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import Header from "../components/header";
 import Content from "../components/content";
 import FormInsert from "../components/form/insert";
 import TodoItem from "../components/todo/item-todo";
 
+const InitialTodos = [
+  { text: 'ir no supermecado', status: false },
+  { text: 'Lavar o carro', status: true }
+]
 
-const Home = () => (
-  // const [todos, setTodo] = useState(0);
+const Home = () => {
 
-  <div style={styles.todo}>
-    <Header />
-    <FormInsert />
-    <Content>
-      <TodoItem key={1} text="Ir na Cozinha" status={false} />
-      <TodoItem key={3} text="Ir no mercado" status={false} />
-      <TodoItem key={2} text="Ir na Cozinha2" status={true} />
-    </Content>
-  </div>
-);
+  const [todos] = useState(InitialTodos);
+
+  const mountTodos = () => {
+    return todos.lenght === 0 ? (
+      <span>Sem resultados</span>
+    ) :
+      (
+        todos.map((todo, i) => (
+          <TodoItem key={i} text={todo.text} status={todo.status} />
+        ))
+      )
+  }
+
+  return (
+    <div style={styles.todo}>
+      <Header />
+      <FormInsert />
+      <Content>
+        {mountTodos()}
+      </Content>
+    </div>
+  );
+}
 
 export default Home;
 
